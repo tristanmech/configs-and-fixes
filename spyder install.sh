f67337d236ec1a9aaafda52a341260dc53690065
tristanmech <tristanmech@users.noreply.github.com>
2018-02-18 09:14:24 +0000

    Update spyder install.sh

--- spyder install.sh
+++ spyder install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 yes | conda remove spyder
-# if you want to remove the config files they are located here /home/pasta/.config/spyder-py3
+# if you want to remove the config files they are located here /home/tristanmech/.config/spyder-py3
 
 yes | conda install spyder
